033e88d84790cfd0fba923e092b15ee8eecc9a1a
roel <roelverbraaken@gmail.com>
2024-04-08 11:55:59 +0200

    jahoo, labels werken

--- language sign.py
+++ language sign.py
@@ -13,20 +13,22 @@ def main():
         # Display the resulting frame
         cv2.imshow('frame', frame)
 
-        # Wait for space key press to take a picture
-        if cv2.waitKey(1) & 0xFF == ord(' '):
-            label = input("Enter label for the image: ")  # Get label from user input
+        # Check for key press
+        key = cv2.waitKey(1) & 0xFF
+
+        # If space key is pressed, take a picture
+        if key == ord('a'):
+            label = 'A' # Get label from user input
             file_name = f"{label}.jpg"  # File name for the image with label
             cv2.imwrite(file_name, frame)  # Save the image with the given label
             print(f"Image saved as {file_name}")
 
-        # Wait for a letter key press to exit the loop
-        if cv2.waitKey(1) & 0xFF != 255:
-            break
+
 
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
